1fa914f9e2a4095b97059450e67a92afa19c65b8
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-03-15 20:45:10 -0300

    Criado Estrutura do Projeto

---
+++ EsudoContext/PagamentoContext.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PagamentoContext.Shared
+{
+    public class Class1
+    {
+    }
+}
---
+++ EsudoContext/PagamentoContext.Testes/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PagamentoContext.Testes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
